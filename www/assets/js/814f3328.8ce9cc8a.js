5578cc7ca531671d9098ffd05ea96569bac0dca5
Tim Schmidt <tim@tapeapp.com>
2022-03-12 15:13:31 +0100

    build and commit output

---
+++ www/assets/js/814f3328.8ce9cc8a.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkdev_portal=self.webpackChunkdev_portal||[]).push([[535],{2014:e=>{e.exports=JSON.parse('{"title":"Recent posts","items":[{"title":"Welcome","permalink":"/blog/welcome"},{"title":"Hello","permalink":"/blog/hello-world"},{"title":"Hola","permalink":"/blog/hola"}]}')}}]);
---
+++ www/assets/js/b2b675dd.cb790377.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkdev_portal=self.webpackChunkdev_portal||[]).push([[533],{5193:e=>{e.exports=JSON.parse('{"permalink":"/blog","page":1,"postsPerPage":10,"totalPages":1,"totalCount":3,"previousPage":null,"nextPage":null,"blogDescription":"Blog","blogTitle":"Blog"}')}}]);
