42164f82ca9c169251c9a225bb25c22dcf1682e8
Tim Schmidt <tim@tapeapp.com>
2022-03-12 15:12:10 +0100

    add src files from other repo

---
+++ apps/dev-portal/docusaurus.config.js
@@ -0,0 +1,95 @@
+module.exports = {
+  title: 'Tape Developers',
+  tagline: 'The tagline of Tape Developers',
+  url: 'https://your-docusaurus-test-site.com',
+  baseUrl: '/',
+  onBrokenLinks: 'throw',
+  onBrokenMarkdownLinks: 'warn',
+  favicon: 'img/favicon.ico',
+  organizationName: 'facebook', // Usually your GitHub org/user name.
+  projectName: 'docusaurus', // Usually your repo name.
+  themeConfig: {
+    navbar: {
+      title: 'Tape Developers',
+      logo: {
+        alt: 'Tape Developers Logo',
+        src: 'img/logo.jpeg',
+      },
+      items: [
+        {
+          to: 'docs/',
+          activeBasePath: 'docs',
+          label: 'Docs',
+          position: 'left',
+        },
+        // { to: 'blog', label: 'Blog', position: 'left' },
+        {
+          href: 'https://tapeapp.com',
+          label: 'Tape',
+          position: 'right',
+        },
+      ],
+    },
+    footer: {
+      style: 'dark',
+      links: [
+        {
+          title: 'Docs',
+          items: [
+            {
+              label: 'Style Guide',
+              to: 'docs/',
+            },
+            {
+              label: 'Second Doc',
+              to: 'docs/doc2/',
+            },
+          ],
+        },
+        {
+          title: 'Community',
+          items: [
+            {
+              label: 'Twitter',
+              href: 'https://twitter.com/Tape_HQ',
+            },
+            {
+              label: 'LinkedIn',
+              href: 'https://www.linkedin.com/company/tapehq',
+            },
+            {
+              label: 'Facebook',
+              href: 'https://www.facebook.com/tapehq',
+            },
+          ],
+        },
+      ],
+      copyright: `Copyright © ${new Date().getFullYear()} Tape Technolgies, Inc.`,
+    },
+    colorMode: {
+      disableSwitch: true
+    }
+  },
+  presets: [
+    [
+      '@docusaurus/preset-classic',
+      {
+        docs: {
+          sidebarPath: require.resolve('./sidebars.js'),
+          // Please change this to your repo.
+          editUrl:
+            'https://github.com/facebook/docusaurus/edit/master/website/',
+        },
+        blog: {
+          showReadingTime: true,
+          // Please change this to your repo.
+          editUrl:
+            'https://github.com/facebook/docusaurus/edit/master/website/blog/',
+        },
+        theme: {
+          customCss: require.resolve('./src/css/custom.css'),
+        },
+      },
+    ],
+  ],
+};
