ce081efdb79eddd4a90a22b8bcc6873718be26d7
Tim Schmidt <tim@tapeapp.com>
2022-03-13 19:44:05 +0100

    refactor

---
+++ apps/dev-portal/src/components/active-user-context-request.js
@@ -0,0 +1,29 @@
+import { DUMMY_ACTIVE_USER_CONTEXT } from './constants';
+
+export function loadActiveUserContextMock() {
+  return fetch('https://jsonplaceholder.typicode.com/posts/1', {}).then(() => {
+    return DUMMY_ACTIVE_USER_CONTEXT;
+  });
+}
+
+const getUserSessionsQuery = `{
+  "operationName": "getUserSessions",
+  "variables": {},
+  "query": "query getUserSessions {  defaultAlias: getUserSessions {    ...UserSessionDtoNoNesting    __typename  }}fragment UserSessionDtoNoNesting on UserSessionDto {  active  userId  createdAt  __typename}"
+}`;
+
+const requestOptions = {
+  method: 'POST',
+  credentials: 'include',
+  headers: { 'Content-Type': 'application/json' },
+  body: getUserSessionsQuery,
+};
+
+export function loadActiveUserContext() {
+  return fetch(
+    'https://mobile.tapeapp.com/graphql/getUserSessions',
+    requestOptions
+  ).then(() => {
+    return DUMMY_ACTIVE_USER_CONTEXT;
+  });
+}
