03346097449369c4f79fab3be0923a7956ccdcfa
Benjamin Assadsolimani <benjamin.assadsolimani@decentro.de>
2022-03-14 10:29:13 +0100

    scale on hover

--- apps/dev-portal/src/components/hexcolor-preview.js
+++ apps/dev-portal/src/components/hexcolor-preview.js
@@ -3,6 +3,7 @@ import React from 'react';
 export default function HexcolorPreview({ color }) {
   return (
     <div
+      class="scale-on-hover"
       style={{
         width: '24px',
         height: '24px',
